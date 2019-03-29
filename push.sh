63e9c8cb21328fb89b9f5db949dbe90e427a59ff
Funky <708863861@qq.com>
2019-03-29 13:10:02 +0800

    update push shell

--- push.sh
+++ push.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-#echo "Build Images..."
-#sudo docker login -u $username -p $password registry.cn-hangzhou.aliyuncs.com
-sudo docker build  --build-arg TF_PACKAGE_VERSION=1.11.0  -t tf-gpu-py3:latest .
-#echo "Push Images..."
-#sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:latest
-#sudo docker logout
+echo "Build Images..."
+sudo docker login -u $username -p $password registry.cn-hangzhou.aliyuncs.com
+sudo docker build  --build-arg TF_PACKAGE_VERSION=1.11.0  -t registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:1.11.0 .
+sudo docker build -t registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:latest .
+
+echo "Push Images..."
+sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:latest
+sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:1.11.0
+sudo docker logout
