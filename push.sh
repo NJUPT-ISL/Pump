3dbe057b946277ba00c4b55ab6220f3107289af6
Funky <708863861@qq.com>
2019-05-17 19:40:36 +0800

    Update dockerfile

--- push.sh
+++ push.sh
@@ -4,5 +4,5 @@ echo "Build Images..."
 sudo docker login -u $username -p $password registry.cn-hangzhou.aliyuncs.com
 sudo docker build -f ./dockerfiles/gpu.Dockerfile -t registry.cn-hangzhou.aliyuncs.com/geekcloud/pytorch-py3-gpu:latest .
 echo "Push Images..."
-sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/pytorch-gpu:latest
+sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/pytorch-py3-gpu:latest
 sudo docker logout
