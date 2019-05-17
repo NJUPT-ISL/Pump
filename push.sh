b8e5c0c9b6b6d6ac28c73649614f019485faaa5e
Funky <708863861@qq.com>
2019-05-17 11:56:16 +0800

    Update dockerfile

--- push.sh
+++ push.sh
@@ -2,13 +2,9 @@
 
 echo "Build Images..."
 sudo docker login -u $username -p $password registry.cn-hangzhou.aliyuncs.com
-sudo docker build -f ./dockerfiles/gpu.Dockerfile --build-arg TF_PACKAGE_VERSION=1.11.0 -t registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:1.11.0 .
 sudo docker build -f ./dockerfiles/gpu.Dockerfile -t registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:latest .
 sudo docker build -f ./dockerfiles/cpu.Dockerfile -t registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-cpu-py3:latest .
-sudo docker build -f ./dockerfiles/cpu.Dockerfile --build-arg TF_PACKAGE_VERSION=1.11.0 -t registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-cpu-py3:1.11.0 .
 echo "Push Images..."
 sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:latest
-sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:1.11.0
 sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-cpu-py3:latest
-sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-cpu-py3:1.11.0
 sudo docker logout
