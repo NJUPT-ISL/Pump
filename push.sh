e9043a65e0a5c81beee152982162e7854a0a4b70
Funky <708863861@qq.com>
2019-06-11 22:14:55 +0800

    fix push.sh

--- push.sh
+++ push.sh
@@ -3,8 +3,6 @@
 echo "Build Images..."
 sudo docker login -u $username -p $password registry.cn-hangzhou.aliyuncs.com
 sudo docker build -f ./dockerfiles/gpu.Dockerfile -t registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:latest .
-sudo docker build -f ./dockerfiles/cpu.Dockerfile -t registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-cpu-py3:latest .
 echo "Push Images..."
 sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:latest
-sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-cpu-py3:latest
 sudo docker logout
