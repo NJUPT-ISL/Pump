fd7824ecb89b08422647b4cd7506d6d6e650e204
Funky <708863861@qq.com>
2019-07-05 13:37:19 +0800

    add branch 2.0.0b1

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 
 echo "Build Images..."
 sudo docker login -u $username -p $password registry.cn-hangzhou.aliyuncs.com
-sudo docker build -f ./dockerfiles/gpu.Dockerfile -t registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:latest .
+sudo docker build -f ./dockerfiles/gpu.Dockerfile --build-arg TF_PACKAGE_VERSION=2.0.0b1 -t registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:2.0.0b1 .
 echo "Push Images..."
-sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:latest
+sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:2.0.0b1
 sudo docker logout
