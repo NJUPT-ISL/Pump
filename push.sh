fba61eac3451892b862ce7cd159f1e06b264d57f
Funky <mrfunky001@gmail.com>
2019-08-26 22:39:54 +0800

    trigger the CI

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 
 echo "Build Images..."
 sudo docker login -u $username -p $password registry.cn-hangzhou.aliyuncs.com
-sudo docker build -f ./dockerfiles/gpu.Dockerfile -t registry.cn-hangzhou.aliyuncs.com/geekcloud/pytorch-py3-gpu:latest .
+sudo docker build -f ./dockerfiles/gpu.Dockerfile -t registry.cn-hangzhou.aliyuncs.com/geekcloud/pytorch-py3-gpu:0.4.0 .
 echo "Push Images..."
-sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/pytorch-py3-gpu:latest
+sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/pytorch-py3-gpu:0.4.0
 sudo docker logout
