432db0fc835b4b41cf6da32a63e3ed1c2b2a6def
Funky <mrfunky001@gmail.com>
2019-08-27 11:33:52 +0800

    update tf package

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 
 echo "Build Images..."
 sudo docker login -u $username -p $password registry.cn-hangzhou.aliyuncs.com
-sudo docker build -f ./dockerfiles/gpu.Dockerfile --build-arg TF_PACKAGE_VERSION=1.14.0 -t registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:1.14.0 .
+sudo docker build -f ./dockerfiles/gpu.Dockerfile --build-arg TF_PACKAGE_VERSION=1.14.0 TF_PACKAGE=tensorflow-gpu -t registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:1.14.0 .
 echo "Push Images..."
 sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:1.14.0
 sudo docker logout
