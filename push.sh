c00013323aa773b2b625b7087a27433054582af2
Funky <mrfunky001@gmail.com>
2019-08-27 12:39:00 +0800

    update bash

--- push.sh
+++ push.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 echo "Build Images..."
 sudo docker login -u $username -p $password registry.cn-hangzhou.aliyuncs.com
 sudo docker build -f ./dockerfiles/gpu.Dockerfile -t registry.cn-hangzhou.aliyuncs.com/geekcloud/pytorch-py3-gpu:0.4.0 .
