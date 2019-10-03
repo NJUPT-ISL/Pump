655732e214a50d4d5320981e4b538dcb78bdfae6
Funky <mrfunky001@gmail.com>
2019-10-04 01:17:06 +0800

    update github action workflows

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Build Images..."
-sudo docker login -u $username -p $password registry.cn-hangzhou.aliyuncs.com
+sudo winpty docker login -u $username -p $password registry.cn-hangzhou.aliyuncs.com
 sudo docker build -f ./dockerfiles/gpu.Dockerfile -t registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:latest .
 echo "Push Images..."
 sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/tf-gpu-py3:latest
